781170e06d5599dd4eeae39be640509a037489c2
NickPater.8 <92941510+nickpater1999@users.noreply.github.com>
2022-03-08 13:19:35 -0500

    CMakeList Fix, sub.cpp edit
    First successful compile using catkin and this version. sub.cpp must now be written to create a subscriber to /camera/color/image_raw to stream data

--- src/sub.cpp
+++ src/sub.cpp
@@ -1,13 +1,13 @@
+#include <sstream>
 #include "ros/ros.h"
 #include "std_msgs/String.h"
-#include <sstream>
 
 int main() {
 	
-	std_msgs::String msg;
-	std::stringstream ss;
-	ss << "It's alive" << count;
-	msg.data = ss.str();
+	// std_msgs::String msg;
+	// std::stringstream ss;
+	// ss << "It's alive" << count;
+	// msg.data = ss.str();
 
-	Return 0;
+	return 0;
 }
