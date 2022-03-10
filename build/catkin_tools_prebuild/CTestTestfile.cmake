8dd6a8d4f6a138ed7902bfe525cdbf3fa01a8f7d
nickpater1999 <nickpater.1999@gmail.com>
2022-03-10 16:22:17 -0500

    Further work on creating subscriber
    Libraries successfully(?) added to script, not much else works right out of box.

---
+++ build/catkin_tools_prebuild/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/pater.8/ws_vis/src/ndi-vision/build/catkin_tools_prebuild
+# Build directory: /home/pater.8/ws_vis/src/ndi-vision/build/catkin_tools_prebuild
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
